6de99fbc9e69a83693e048b9c4f7e7de3cb3a09f
umuttsaltan <umtcns99@gmail.com>
2023-07-25 23:26:34 +0300

    Initial commit

---
+++ src/main/java/com/alperumut/resourcesharing/ResourceSharingApplication.java
@@ -0,0 +1,13 @@
+package com.alperumut.resourcesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alperumut/resourcesharing/ResourceSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperumut.resourcesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
